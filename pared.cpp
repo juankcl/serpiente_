e1b5b7318ee64e2f7ef46bf2fabf85dae0baf62d
MiguelGtz <51221186+MiguelGtz@users.noreply.github.com>
2019-05-31 15:03:48 -0500

    Comentarios

--- pared.cpp
+++ pared.cpp
@@ -3,7 +3,7 @@
 
 pared::pared()
 {
-    // draw
+    // dibujar el objeto pared
     setRect(0,0,20,600);
     QBrush brush;
     brush.setStyle(Qt::SolidPattern);
@@ -12,12 +12,13 @@ pared::pared()
 
 }
 
+//cambio de orientacion de objeto pared
 void pared::vertical()
 {
     setRect(0,0,800,20);
     QBrush brush;
-    brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::yellow);
+    brush.setStyle(Qt::SolidPattern); //color solido
+    brush.setColor(Qt::yellow); //pared es color amarillo
     setBrush(brush);
 
 }
